0952bc876739b090215692700959a672bcaa1d72
Rioba-Ian <riobafelix@gmail.com>
2025-06-04 15:40:36 +0300

    add difficulty level for game

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -20,6 +21,7 @@ var (
 type Game struct {
 	answer     int
 	difficulty int
+	chances    int
 }
 
 func loadGame() *Game {
@@ -27,14 +29,14 @@ func loadGame() *Game {
 
 	game.answer = randRange(*low, *high)
 
-	fmt.Println("The answer is", game.answer)
-
 	return &game
 }
 
-func (game *Game) runGame() {
+func (game *Game) runGame(wg *sync.WaitGroup) {
 	timer := time.NewTicker(time.Second * time.Duration(*timeLimit))
 	done := make(chan bool)
+	defer wg.Done()
+	wg.Add(2)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -42,6 +44,7 @@ func (game *Game) runGame() {
 	numTries := 0
 
 	go func() {
+		game.Difficulty()
 
 		fmt.Println("Make your guess:")
 	gameLoop:
@@ -78,19 +81,50 @@ func (game *Game) runGame() {
 	select {
 	case <-done:
 	case <-timer.C:
-		fmt.Println("time's up, too bad you didn't get it.")
+		fmt.Println("time's up, too bad you didn't get it. The answer was ", game.answer)
+	}
+
+}
+
+func (game Game) Difficulty() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println(`Please select the difficulty level:
+			1. Easy (10 chances)
+			2. Medium (5 chances)
+			3. Hard (3 chances)`)
+	fmt.Printf("Enter your choice: ")
+	scanner.Scan()
+	userDifficultyAnswer := scanner.Text()
+	difficultyInt, err := strconv.Atoi(userDifficultyAnswer)
+	fatalError("Failed to parse difficulty level: ", err)
+
+	game.difficulty = difficultyInt
+
+	switch game.difficulty {
+	case 1:
+		game.chances = 10
+		fmt.Println("Great! You have selected the Easy level")
+	case 2:
+		game.chances = 5
+		fmt.Println("Great! You have selected the Medium level")
+	case 3:
+		game.chances = 3
+		fmt.Println("Great! You have selected the Hard level")
 	}
 
 }
 
 func main() {
+	var wg sync.WaitGroup
 	fmt.Println(`
 		Welcome to the Number Guessing Game!
 		I'm thinking of a number between 1 and 100.
 		You have 5 chances to guess the correct number.
 		`)
 	game := loadGame()
-	game.runGame()
+	game.runGame(&wg)
+
 }
 
 func randRange(min, max int) int {
