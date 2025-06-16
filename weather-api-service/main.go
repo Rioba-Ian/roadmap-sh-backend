aee7fe7742dc472e6f36a3507fb1699b81a7efa0
Rioba-Ian <riobafelix@gmail.com>
2025-06-16 17:58:13 +0300

    fix docker path for .env file

--- weather-api-service/main.go
+++ weather-api-service/main.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	// Load environment variables at startup with the directory being ./weather-api-service
+	// This is optional - if .env file doesn't exist (like in Docker), environment variables
+	// should already be provided by the container runtime
 	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("Error loading .env file:", err)
+		log.Println("Warning: Could not load .env file (this is normal in Docker):", err)
 	}
 
 	weatherApi := APIServer{
