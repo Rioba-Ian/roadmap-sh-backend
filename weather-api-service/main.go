8e93f4f8d5c1f322f1853c95eef8f83fa8432fa4
Rioba-Ian <riobafelix@gmail.com>
2025-06-16 17:33:26 +0300

    change docker file directory in dotend load

--- weather-api-service/main.go
+++ weather-api-service/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Load environment variables at startup with the directory being ./weather-api-service
-	if err := godotenv.Load("./weather-api-service/.env"); err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error loading .env file:", err)
 	}
 
