f77e377a259c0b547e8ca232573d2df40dc32f86
Rioba-Ian <riobafelix@gmail.com>
2025-12-02 15:46:51 +0300

    feat: middleware for checking Auth header Bearer token

---
+++ expense-tracker-api/cmd/middlewares/middleware.go
@@ -0,0 +1,38 @@
+package middlewares
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Rioba-Ian/expense-tracker-api/helpers"
+)
+
+func Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header required", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := parts[1]
+		claims, err := helpers.ValidateToken(tokenString)
+		if err != nil {
+			log.Printf("Token validation error: %w\n", err)
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "userID", claims.ID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
+}
