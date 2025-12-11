546872727605fb053df9bc80425b2b448d8cf6db
RiobaIan <ian.rioba@globallinkplus.com>
2025-12-11 12:55:49 +0300

    feat: add indexes for users: email and expenses: id

--- expense-tracker-api/db/migrations/20251211094937_alter_users_table_blacklist.sql
+++ expense-tracker-api/db/migrations/20251211094937_alter_users_table_blacklist.sql
@@ -1,6 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE users ADD blacklist TEXT;
+
+CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_expenses_id ON expenses(id);
 -- +goose StatementEnd
 
 -- +goose Down
