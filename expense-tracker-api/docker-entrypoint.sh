ad04553dfe233aac10e396e222016c174ef9b9a7
Rioba-Ian <riobafelix@gmail.com>
2025-12-18 17:33:10 +0300

    upgrade to v0.3

--- expense-tracker-api/docker-entrypoint.sh
+++ expense-tracker-api/docker-entrypoint.sh
@@ -8,7 +8,7 @@ sleep 5
 # Run the goose migrations
 echo "Running database migrations with Goose..."
 export GOOSE_DRIVER=postgres
-export GOOSE_DBSTRING=postgres://${DB_USER}:${DB_PASSWORD}@postgres_db:5432/${DB_NAME}?sslmode=disable
+export GOOSE_DBSTRING=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable
 goose -version
 echo "Using connection string: ${GOOSE_DBSTRING}"
 goose status
